e9864a9ec34044eb5a32943e2d2e31a1533de764
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-06-12 17:52:24 +0300

    refactoring of function in categories(final)

--- src/js/book-card.js
+++ src/js/book-card.js
@@ -51,7 +51,10 @@ async function contentLoad() {
     .join('');
   heroRefs.catsList.classList.remove('category-all-books');
   heroRefs.catsList.classList.add('categories-prewiews', 'books-list-top');
+  heroRefs.her;
   heroRefs.catsList.innerHTML = `${homeMarkup}`;
+  document.querySelector('.hero-heading').innerHTML =
+    '<h1 class="hero-heading">Best Sellers<span class="heading-painter">Books</span> </h1>';
   setTimeout(() => {
     heroRefs.openCategoryBtn = document.querySelectorAll('.btn-loadmore');
     heroRefs.openCategoryBtn.forEach(btn =>
