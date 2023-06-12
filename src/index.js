aa8a23884993bec2e8b894d728ff128629725b0e
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-06-12 13:48:29 +0300

    changed import shopping_list.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import './js/all_categories.js';
 import './js/book-card.js';
 import './js/header.js';
 import './js/pop-up.js';
-import './js/shopping_list.js';
 import './js/support_Ukraine.js';
 import './js/theme-switcher.js';
